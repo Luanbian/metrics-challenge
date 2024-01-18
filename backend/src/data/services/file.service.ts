0598ffe2322ed6d29d44ba9a5d91c0f33452ce35
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-18 13:16:16 -0300

    feat: format timestamp

--- backend/src/data/services/file.service.ts
+++ backend/src/data/services/file.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import * as xlsx from 'xlsx';
 import * as csvParser from 'csv-parser';
 import * as fs from 'fs';
+import { format, isValid } from 'date-fns';
 
 @Injectable()
 export class FileService {
@@ -19,7 +20,8 @@ export class FileService {
     const workbook = xlsx.readFile(path);
     const sheetName = workbook.SheetNames[0];
     const jsonData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
-    return jsonData;
+    const formatedJson = this.convertDateToString(jsonData);
+    return formatedJson;
   }
 
   private async csvToJson(path: string): Promise<unknown[]> {
@@ -32,4 +34,36 @@ export class FileService {
         .on('error', error => reject(error));
     });
   }
+
+  private convertDateToString(json: unknown[]): unknown[] {
+    json.forEach(item => {
+      item["data início"] = this.convertAndFormatDate(item["data início"], "data início");
+      item["data status"] = this.convertAndFormatDate(item["data status"], "data status");
+      item["data cancelamento"] = this.convertAndFormatDate(item["data cancelamento"], "data cancelamento");
+    });
+    return json;
+  }
+
+  private convertAndFormatDate = (value: number | null | undefined, fieldName: string): string | null => {
+    const secondsPerDay = 86400;
+    const milisecondsPerSecond = 1000;
+    const dateFormat = 'd/M/yy HH:mm';
+  
+    if (value !== null && value !== undefined) {
+      try {
+        const timestamp = value * secondsPerDay * milisecondsPerSecond;
+        if (timestamp > Number.MAX_SAFE_INTEGER || timestamp < Number.MIN_SAFE_INTEGER) {
+          return null;
+        }
+        const date = new Date(timestamp);
+        if (!isValid(date)) {
+          return null;
+        }
+        return format(date, dateFormat);
+      } catch (error) {
+        console.error(`Erro ao converter data para o campo "${fieldName}": `, error);
+      }
+    }
+    return null;
+  };
 }
