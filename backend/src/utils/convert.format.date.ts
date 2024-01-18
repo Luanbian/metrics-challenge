fb00a00bc2a9cddd7162f9f07239e05cbb00550f
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-18 13:51:06 -0300

    fix: error to convert timestamp

--- backend/src/utils/convert.format.date.ts
+++ backend/src/utils/convert.format.date.ts
@@ -1,13 +1,14 @@
 import { format, isValid } from "date-fns";
 
 export const convertAndFormatDate = (value: number | null | undefined, fieldName: string): string | null => {
+  const excelBaseDate = new Date('1899-12-30').getTime();
   const secondsPerDay = 86400;
   const milisecondsPerSecond = 1000;
   const dateFormat = 'd/M/yy HH:mm';
 
   if (value !== null && value !== undefined) {
     try {
-      const timestamp = value * secondsPerDay * milisecondsPerSecond;
+      const timestamp = value * secondsPerDay * milisecondsPerSecond + excelBaseDate;
       if (timestamp > Number.MAX_SAFE_INTEGER || timestamp < Number.MIN_SAFE_INTEGER) {
         return null;
       }
