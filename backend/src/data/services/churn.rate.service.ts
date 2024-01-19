4557ae8eb3e67b1b8d4c344670f633a090d715f7
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-19 16:20:29 -0300

    feat: create churn rate service

---
+++ backend/src/data/services/churn.rate.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from "@nestjs/common";
+import { IExcelModel } from "../mapper/excel.mapper";
+
+export interface SignatureStatus {
+  actives: IExcelModel[]
+  overDues: IExcelModel[]
+  canceleds: IExcelModel[]
+  canceledsTrial: IExcelModel[]
+  upgrades: IExcelModel[]
+}
+
+@Injectable()
+export class ChurnRateService {
+  public async metrics (json: IExcelModel[]) {
+    const general = this.separateActivesAndCanceleds(json);
+    return {
+      general
+    }
+  }
+
+  private separateActivesAndCanceleds (json: IExcelModel[]) {
+    const separate: SignatureStatus = {
+      actives: [],
+      canceleds: [],
+      canceledsTrial: [],
+      overDues: [],
+      upgrades: []
+    }
+    for (const item of json) {
+      const { status } = item;
+
+      switch (status) {
+        case 'Ativa':
+          separate.actives.push(item);
+          break;
+        case 'Cancelada':
+          separate.canceleds.push(item);
+          break;
+        case 'Trial cancelado':
+          separate.canceledsTrial.push(item);
+          break;
+        case 'Upgrade':
+          separate.upgrades.push(item);
+          break;
+        default:
+          throw new Error(`Status do ${item} não reconhecido`);
+      }
+    }
+    return separate;
+  }
+}
