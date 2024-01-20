944ae220254592c583db71712376c46a5128a25b
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-20 14:30:20 -0300

    feat: first version front end

---
+++ frontend/src/layout/dashboard/DashboardLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="wrapper">
+    <side-bar>
+      <template slot="links">
+        <sidebar-link to="/dashboard" :name="$t('sidebar.dashboard')" icon="tim-icons icon-chart-pie-36"/>
+        <sidebar-link to="/icons" :name="$t('sidebar.icons')" icon="tim-icons icon-atom"/>
+        <sidebar-link to="/maps" :name="$t('sidebar.maps')" icon="tim-icons icon-upload"/>
+      </template>
+    </side-bar>
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+
+      <dashboard-content @click.native="toggleSidebar">
+
+      </dashboard-content>
+
+      <content-footer></content-footer>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+</style>
+<script>
+import TopNavbar from "./TopNavbar.vue";
+import DashboardContent from "./Content.vue";
+export default {
+  components: {
+    TopNavbar,
+    DashboardContent,
+  },
+  methods: {
+    toggleSidebar() {
+      if (this.$sidebar.showSidebar) {
+        this.$sidebar.displaySidebar(false);
+      }
+    }
+  }
+};
+</script>
