cd628b9791598fb77a69327a513ab047102fb3c1
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-20 22:51:53 -0300

    style: improve form to send file

--- frontend/src/pages/Maps.vue
+++ frontend/src/pages/Maps.vue
@@ -1,9 +1,18 @@
 <template>
   <card type="plain" title="Upload file">
-    <form @submit.prevent>
-      <input type="file"/>
-      <small slot="helperText" id="fileHelper" class="form-text text-muted">.csv or .xlsx</small>
-      <base-button native-type="submit" type="primary">Enviar</base-button>
-    </form>
+    <div>
+      <div class="input-group mb-3">
+        <div class="custom-file">
+          <input type="file" class="custom-file-input" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04">
+          <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
+        </div>
+      </div>
+      <div class="mb-3">
+        <small id="emailHelp" class="form-text text-muted">.csv or .xlsx</small>
+      </div>
+      <div class="d-flex justify-content-end">
+        <base-button native-type="submit" type="primary">Submit</base-button>
+      </div>
+    </div>
   </card>
 </template>
