2fa191a00dd5a3d8044f224685530840cb147f59
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-22 13:31:07 -0300

    fix: clean folders

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,11 +1,7 @@
 import BaseInput from "./Inputs/BaseInput.vue";
 
-import BaseRadio from "./BaseRadio.vue";
 import BaseTable from "./BaseTable.vue";
 import BaseButton from "./BaseButton";
-import BaseNav from "./BaseNav";
-import Modal from "./Modal";
-import CloseButton from "./CloseButton";
 
 import Card from "./Cards/Card.vue";
 import StatsCard from "./Cards/StatsCard.vue";
@@ -15,12 +11,8 @@ import SidebarPlugin from "./SidebarPlugin/index";
 export {
   BaseInput,
   Card,
-  Modal,
-  CloseButton,
   StatsCard,
   BaseTable,
-  BaseRadio,
   BaseButton,
   SidebarPlugin,
-  BaseNav
 };
