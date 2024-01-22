676c2a70ceaad7e2fde695fbe3bcbb5e17bf7e8b
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-22 16:05:19 -0300

    test: upload a file

--- backend/src/presentation/tests/file.controller.spec.ts
+++ backend/src/presentation/tests/file.controller.spec.ts
@@ -1,37 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
+import * as fs from 'fs';
+import * as path from 'path';
 import { FileController } from '../controllers/file.controller';
 import { MetricsSignatureService } from '../../data/services/metrics.signature.service';
 import { ChurnRateService } from '../../data/services/churn.rate.service';
 import { NestApplication } from '@nestjs/core';
+import { MetricsYearService } from '../../data/services/metrics.year.service';
+import { MetricsMonthService } from '../../data/services/metrics.month.service';
 
 describe('FileController', () => {
   let app: NestApplication;
-  let sut: FileController;
-  let mrrService: MetricsSignatureService;
-  let churnService: ChurnRateService;
 
   beforeEach(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
       controllers: [FileController],
-      providers: [MetricsSignatureService, ChurnRateService],
+      providers: [MetricsSignatureService, MetricsYearService, MetricsMonthService, ChurnRateService],
     }).compile();
 
-    const app = moduleRef.createNestApplication();
+    app = moduleRef.createNestApplication();
     await app.init();
-    mrrService = moduleRef.get<MetricsSignatureService>(MetricsSignatureService);
-    churnService = moduleRef.get<ChurnRateService>(ChurnRateService);
   });
 
   it('should upload a file', async () => {
+    const filePath = path.join(__dirname, 'model_test.csv');
     const response = await request(app.getHttpServer())
       .post('/api/file')
-      .attach('file', Buffer.from('file content'), {
+      .attach('file', fs.readFileSync(filePath), {
         filename: 'modelo_test.csv',
         contentType: 'text/csv'
       });
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('MRR');
-    expect(response.body).toHaveProperty('churn');
+    expect(response.status).toBe(201);
   });
 });
