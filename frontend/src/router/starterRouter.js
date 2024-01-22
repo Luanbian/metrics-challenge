9813699ce8361a75db3844c352b22bd8dcbee33c
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-22 19:30:37 -0300

    feat: turn maps the main page

--- frontend/src/router/starterRouter.js
+++ frontend/src/router/starterRouter.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import DashboardLayout from '../layout/starter/SampleLayout.vue';
-import Starter from '../layout/starter/SamplePage.vue';
 
 Vue.use(Router);
 
@@ -10,12 +8,12 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      redirect: '/dashboard',
+      redirect: '/maps',
       component: DashboardLayout,
       children: [
         {
-          path: 'dashboard',
-          name: 'dashboard',
+          path: 'maps',
+          name: 'maps',
           components: { default: Starter }
         }
       ]
